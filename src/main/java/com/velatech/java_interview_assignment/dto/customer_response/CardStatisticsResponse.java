4f554a022d4a0692f0d5a95b44b0963bbea43d3d
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-12-08 17:29:42 +0100

    Create Card statistics response

---
+++ src/main/java/com/velatech/java_interview_assignment/dto/customer_response/CardStatisticsResponse.java
@@ -0,0 +1,16 @@
+package com.velatech.java_interview_assignment.dto.customer_response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+public class CardStatisticsResponse {
+    private boolean success;
+    private int start;
+    private int limit;
+    private long size;
+    private Map<String, Integer> payload;
+}
