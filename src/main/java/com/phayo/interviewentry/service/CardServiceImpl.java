67b33c8ab742d3509b9262ac387f60ae1b970f1c
phayo <anazodogodwin@gmail.com>
2019-12-08 17:08:03 +0100

    create method logCardVerificationRequest in CardServiceImpl Class to log each request made from client

--- src/main/java/com/phayo/interviewentry/service/CardServiceImpl.java
+++ src/main/java/com/phayo/interviewentry/service/CardServiceImpl.java
@@ -2,8 +2,11 @@ package com.phayo.interviewentry.service;
 
 import com.phayo.interviewentry.dto.client_response.CardVerifyResponseDto;
 import com.phayo.interviewentry.exception.InvalidInputException;
+import com.phayo.interviewentry.model.CardVerificationRequestRecord;
+import com.phayo.interviewentry.repository.CardVerificationRequestRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,11 @@ import java.util.Optional;
 
 public class CardServiceImpl implements CardService {
 
+    private Logger log = LoggerFactory.getLogger(CardServiceImpl.class);
+
+    @Autowired
+    CardVerificationRequestRepository verificationRequestRepository;
+
     /**
      * @Params: cardNumber:String
      * @Returns: cardNumber:String
@@ -21,7 +29,6 @@ public class CardServiceImpl implements CardService {
      * a truncated 6 digit String otherwise.
      */
     private String validateCardNumber(String cardNumber) throws InvalidInputException{
-        private Logger log = LoggerFactory.getLogger(CardServiceImpl.class);
 
         if ( !cardNumber.matches("\\d{6,19}")){
             throw new InvalidInputException("Card Number Input is not valid");
@@ -35,10 +42,16 @@ public class CardServiceImpl implements CardService {
         return cardNumber;
     }
 
+    private void logCardVerificationRequest(String validCardNumber) {
+        log.info("Saved a card verification request to database");
+        verificationRequestRepository.save(new CardVerificationRequestRecord(validCardNumber));
+    }
+
     @Override
     public CardVerifyResponseDto executeCardVerification(String cardNumber, HttpEntity<?> entity) throws HttpStatusCodeException, InvalidInputException {
+        // Validate the card number in the request
         String validCardNumber = validateCardNumber(cardNumber);
 
-
+        logCardVerificationRequest(validCardNumber);
     }
 }
