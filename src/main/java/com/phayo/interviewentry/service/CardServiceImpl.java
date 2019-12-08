54164d495fcb879aa0a34387746a216c5cea3403
phayo <anazodogodwin@gmail.com>
2019-12-08 17:20:36 +0100

    create method mapToCardVerifyResponseDto for converting the stored record saved in database to the desirable client response object

--- src/main/java/com/phayo/interviewentry/service/CardServiceImpl.java
+++ src/main/java/com/phayo/interviewentry/service/CardServiceImpl.java
@@ -1,8 +1,12 @@
 package com.phayo.interviewentry.service;
 
+import com.phayo.interviewentry.dto.client_response.CardVerifyPayloadDto;
 import com.phayo.interviewentry.dto.client_response.CardVerifyResponseDto;
+import com.phayo.interviewentry.dto.external_api_response.BinListResponse;
 import com.phayo.interviewentry.exception.InvalidInputException;
+import com.phayo.interviewentry.model.CardDetailEntity;
 import com.phayo.interviewentry.model.CardVerificationRequestRecord;
+import com.phayo.interviewentry.repository.CardDetailEntityRepository;
 import com.phayo.interviewentry.repository.CardVerificationRequestRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +24,9 @@ public class CardServiceImpl implements CardService {
 
     @Autowired
     CardVerificationRequestRepository verificationRequestRepository;
+
+    @Autowired
+    CardDetailEntityRepository cardDetailEntityRepository;
 
     /**
      * @Params: cardNumber:String
@@ -47,11 +54,45 @@ public class CardServiceImpl implements CardService {
         verificationRequestRepository.save(new CardVerificationRequestRecord(validCardNumber));
     }
 
+    /**
+     * Transforms the BinList rsponse to the CardVerifyResponseDto which meets our clients expectation
+     * @param cardDetailEntity Object of CardDetail stored on the Database
+     * @return CardVerifyResponseDto
+     */
+    private CardVerifyResponseDto mapToCardVerifyResponseDto(CardDetailEntity cardDetailEntity){
+        CardVerifyResponseDto cardVerifyResponseDto = new CardVerifyResponseDto();
+        CardVerifyPayloadDto cardVerifyPayloadDto = new CardVerifyPayloadDto();
+
+        if(cardDetailEntity != null){
+            cardVerifyResponseDto.setPayload(cardVerifyPayloadDto);
+
+            cardVerifyResponseDto.getPayload()
+                    .setBank(cardDetailEntity.getBank() == null ? "" : cardDetailEntity.getBank());
+            cardVerifyResponseDto.getPayload()
+                    .setScheme(cardDetailEntity.getScheme() == null ? "" : cardDetailEntity.getScheme());
+            cardVerifyResponseDto.getPayload()
+                    .setType(cardDetailEntity.getBrand() == null ? "" : cardDetailEntity.getBrand());
+        }
+
+        return cardVerifyResponseDto;
+    }
+
     @Override
     public CardVerifyResponseDto executeCardVerification(String cardNumber, HttpEntity<?> entity) throws HttpStatusCodeException, InvalidInputException {
         // Validate the card number in the request
         String validCardNumber = validateCardNumber(cardNumber);
 
+        // Log the request to database
         logCardVerificationRequest(validCardNumber);
+
+        BinListResponse binListResponse = null;
+        ResponseEntity<BinListResponse> response = null;
+
+        // Checks if a request has been previously made to the external API and return the saved response
+        Optional<CardDetailEntity> savedResponse = cardDetailEntityRepository.findByCardNumber(validCardNumber);
+        if (savedResponse.isPresent()){
+            log.info("There was a saved response");
+            return mapToCardVerifyResponseDto(savedResponse.get());
+        }
     }
 }
