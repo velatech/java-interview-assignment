369ec050629e0eb462cfa1f1aa596c9d177eb6a1
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-12-08 15:02:08 +0100

    Initial commit

---
+++ src/test/java/com/velatech/java_interview_assignment/JavaInterviewAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velatech.java_interview_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInterviewAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
