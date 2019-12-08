d0b8cb09a93be03ff948dd056fafce6d7c5bab3b
phayo <anazodogodwin@gmail.com>
2019-12-08 18:06:58 +0100

    create customquery for method getVerificationRequestRecordGroupedByCardNumber in the CardVerificationRequestRepository interface

--- src/main/java/com/phayo/interviewentry/repository/CardVerificationRequestRepository.java
+++ src/main/java/com/phayo/interviewentry/repository/CardVerificationRequestRepository.java
@@ -1,10 +1,16 @@
 package com.phayo.interviewentry.repository;
 
 import com.phayo.interviewentry.model.CardVerificationRequestRecord;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 @Repository
 public interface CardVerificationRequestRepository extends JpaRepository<CardVerificationRequestRecord, Long> {
-
+    @Query("SELECT record.cardNumber AS cardNumber, COUNT(record.cardNumber) AS count FROM CardVerificationRequestRecord record GROUP BY record.cardNumber")
+    Page<Map<String, Object>> getVerificationRequestRecordGroupedByCardNumber(Pageable pageable);
 }
