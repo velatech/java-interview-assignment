69158dac736f0c9633d068a308ffe8c452e49749
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-12-08 17:59:07 +0100

    Set headers content type to expect JSON

--- src/main/java/com/velatech/java_interview_assignment/controller/CardSchemeController.java
+++ src/main/java/com/velatech/java_interview_assignment/controller/CardSchemeController.java
@@ -1,8 +1,7 @@
 package com.velatech.java_interview_assignment.controller;
 
 import com.velatech.java_interview_assignment.dto.customer_response.CardVerificationResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +13,9 @@ public class CardSchemeController {
 
     @GetMapping("/verify/{cardNumber}")
     public ResponseEntity<CardVerificationResponse> verifyCard(@PathVariable String cardNumber){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<?> entity = new HttpEntity<>(headers);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
