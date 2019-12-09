0946e10a176050736b40552d7c6fcca89fd5ed5e
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-12-09 03:18:20 +0100

    Create Global Exception Handler

---
+++ src/main/java/com/velatech/java_interview_assignment/exception/GlobalExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.velatech.java_interview_assignment.exception;
+
+import com.velatech.java_interview_assignment.dto.customer_response.CardVerificationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(value = {InvalidInputExceptionHandler.class})
+    public ResponseEntity<?> invalidInputException(InvalidInputExceptionHandler ex, WebRequest request){
+        logger.error(ex.getMessage());
+        CardVerificationResponse cardVerificationResponse = new CardVerificationResponse();
+        cardVerificationResponse.setSuccess(false);
+        cardVerificationResponse.setPayload(null);
+        return new ResponseEntity<>(cardVerificationResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {InvalidPageExceptionHandler.class})
+    public ResponseEntity<?> invalidInputException(InvalidPageExceptionHandler ex, WebRequest request){
+        logger.error(ex.getMessage());
+        CardVerificationResponse cardVerificationResponse = new CardVerificationResponse();
+        cardVerificationResponse.setSuccess(false);
+        cardVerificationResponse.setPayload(null);
+        return new ResponseEntity<>(cardVerificationResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {org.springframework.web.client.HttpStatusCodeException.class})
+    public ResponseEntity<?> statusCodeException(HttpStatusCodeException ex, WebRequest request){
+        logger.error(ex.getMessage());
+        CardVerificationResponse cardVerificationResponse = new CardVerificationResponse();
+        cardVerificationResponse.setSuccess(false);
+        cardVerificationResponse.setPayload(null);
+        return new ResponseEntity<>(cardVerificationResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> globalExceptionHandler(Exception ex, WebRequest request) {
+        logger.error(ex.getMessage());
+        CardVerificationResponse cardVerificationResponse = new CardVerificationResponse();
+        cardVerificationResponse.setSuccess(false);
+        cardVerificationResponse.setPayload(null);
+        return new ResponseEntity<>(cardVerificationResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
