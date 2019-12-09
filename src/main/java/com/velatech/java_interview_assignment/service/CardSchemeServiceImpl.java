604e046818a8e13d3403c45039bc0ee1f4c2bed2
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-12-09 09:59:44 +0100

    Create functions for logging verification records and validating card number length

--- src/main/java/com/velatech/java_interview_assignment/service/CardSchemeServiceImpl.java
+++ src/main/java/com/velatech/java_interview_assignment/service/CardSchemeServiceImpl.java
@@ -1,11 +1,15 @@
 package com.velatech.java_interview_assignment.service;
 
+import com.velatech.java_interview_assignment.exception.InvalidInputException;
+import com.velatech.java_interview_assignment.model.CardVerificationRecord;
+import com.velatech.java_interview_assignment.repository.CardDetailRepository;
 import com.velatech.java_interview_assignment.repository.CardVerificationRecordRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 
 @Service
@@ -17,6 +21,42 @@ public class CardSchemeServiceImpl {
 
     @Autowired
     CardVerificationRecordRepository cardVerificationRecordRepository;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    CardDetailRepository cardDetailRepository;
+
+    /**
+     * This function logs a new verification request to the database
+     * @param cardNumber:String
+     */
+    private void logCardVerificationRecord(String cardNumber) {
+        log.info("Logged a new verification request to the database");
+        cardVerificationRecordRepository.save(new CardVerificationRecord(cardNumber));
+    }
+
+    /**
+     * This function validates the length of the card
+     * @param cardNumber
+     * @return the card number (truncated to 6 digits) if the input falls within the range of 6 to 19 characters
+     * @throws InvalidInputException if the input is invalid
+     */
+    private String validateCardNumberLength(String cardNumber) throws InvalidInputException{
+
+        if ( !cardNumber.matches("\\d{6,19}")){
+            throw new InvalidInputException("Invalid Card Number Input");
+        }
+
+        log.info("Card Validated");
+        if (cardNumber.length() > 6){
+            return cardNumber.substring(0, 6);
+        }
+
+        return cardNumber;
+    }
+
 
 
 }
