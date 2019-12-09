ec6a968e921cea2e61f03aeb774885b21d2a7fdb
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-12-09 03:36:18 +0100

    Create Card Scheme Service interface

---
+++ src/main/java/com/velatech/java_interview_assignment/service/CardSchemeService.java
@@ -0,0 +1,17 @@
+package com.velatech.java_interview_assignment.service;
+
+import com.velatech.java_interview_assignment.dto.customer_response.CardStatisticsResponse;
+import com.velatech.java_interview_assignment.dto.customer_response.CardVerificationResponse;
+import com.velatech.java_interview_assignment.exception.InvalidInputExceptionHandler;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
+
+import java.awt.print.Pageable;
+
+@Service
+public interface CardSchemeService {
+    CardVerificationResponse performCardVerification(String cardNumber, HttpEntity<?> entity) throws HttpStatusCodeException, InvalidInputExceptionHandler;
+
+    CardStatisticsResponse getCardVerificationRecord(Pageable pageable) throws RuntimeException;
+}
