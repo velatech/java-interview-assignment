13fe2f126098fe28a91ba0c946aae2f215460309
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2020-01-19 20:51:18 +0100

    Create proper error messages

--- src/main/java/com/velatech/java_interview_assignment/exception/GlobalExceptionHandler.java
+++ src/main/java/com/velatech/java_interview_assignment/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.velatech.java_interview_assignment.exception;
 
 import com.velatech.java_interview_assignment.dto.customer_response.CardVerificationResponse;
+import com.velatech.java_interview_assignment.dto.customer_response.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -18,37 +19,37 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = {InvalidInputException.class})
     public ResponseEntity<?> invalidInputException(InvalidInputException ex, WebRequest request){
         logger.error(ex.getMessage());
-        CardVerificationResponse cardVerificationResponse = new CardVerificationResponse();
-        cardVerificationResponse.setSuccess(false);
-        cardVerificationResponse.setPayload(null);
-        return new ResponseEntity<>(cardVerificationResponse, HttpStatus.BAD_REQUEST);
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setError(HttpStatus.BAD_REQUEST.toString());
+        errorResponse.setMessage("Invalid Input ... Please try again");
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = {InvalidPageException.class})
-    public ResponseEntity<?> invalidInputException(InvalidPageException ex, WebRequest request){
+    public ResponseEntity<?> invalidPageException(InvalidPageException ex, WebRequest request){
         logger.error(ex.getMessage());
-        CardVerificationResponse cardVerificationResponse = new CardVerificationResponse();
-        cardVerificationResponse.setSuccess(false);
-        cardVerificationResponse.setPayload(null);
-        return new ResponseEntity<>(cardVerificationResponse, HttpStatus.BAD_REQUEST);
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setError(HttpStatus.BAD_REQUEST.toString());
+        errorResponse.setMessage("Invalid Input ... Please try again");
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = {org.springframework.web.client.HttpStatusCodeException.class})
     public ResponseEntity<?> statusCodeException(HttpStatusCodeException ex, WebRequest request){
         logger.error(ex.getMessage());
-        CardVerificationResponse cardVerificationResponse = new CardVerificationResponse();
-        cardVerificationResponse.setSuccess(false);
-        cardVerificationResponse.setPayload(null);
-        return new ResponseEntity<>(cardVerificationResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setError(HttpStatus.INTERNAL_SERVER_ERROR.toString());
+        errorResponse.setMessage("Sorry ... It's not your fault ... It's from us");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> globalExceptionHandler(Exception ex, WebRequest request) {
         logger.error(ex.getMessage());
-        CardVerificationResponse cardVerificationResponse = new CardVerificationResponse();
-        cardVerificationResponse.setSuccess(false);
-        cardVerificationResponse.setPayload(null);
-        return new ResponseEntity<>(cardVerificationResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setError(HttpStatus.INTERNAL_SERVER_ERROR.toString());
+        errorResponse.setMessage("Sorry ... It's not your fault ... It's from us");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
