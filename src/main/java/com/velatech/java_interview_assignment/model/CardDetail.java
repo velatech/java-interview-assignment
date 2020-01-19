329f05e2457fbaac9dd88fdee8bfb096fee4d37b
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2020-01-19 19:08:15 +0100

    Undo card detail adjustment for now

--- src/main/java/com/velatech/java_interview_assignment/model/CardDetail.java
+++ src/main/java/com/velatech/java_interview_assignment/model/CardDetail.java
@@ -16,10 +16,20 @@ public class CardDetail {
     @Column(name = "card_number")
     private String cardNumber;
 
+    @Column(name = "card_number_length")
+    private int cardNumberLength;
+
+    @Column(name = "luhn_check")
+    private boolean luhn;
+
     private String scheme;
+
+    private String brand;
 
     @Enumerated(EnumType.STRING)
     private CardType type;
+
+    private String country;
 
     private String bank;
 
